659c074ece754772fb155920036d03d1cf5d7c15
David Forshaw <dfcv126@gmail.com>
2013-11-26 18:05:15 -0800

    chap 5

---
+++ src/test/java/com/javafortesters/testwithourownclasses/domainobject/TestAppEnvironmentTest.java
@@ -0,0 +1,11 @@
+package com.javafortesters.testwithourownclasses.domainobject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: David Forshaw
+ * Date: 11/26/13
+ * Time: 5:33 PM
+ */
+
+public class TestAppEnvironmentTest {
+}
