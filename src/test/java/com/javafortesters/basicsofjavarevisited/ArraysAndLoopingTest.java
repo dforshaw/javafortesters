29c1d54e857a1540922ec0e14e0a4d594659e958
David Forshaw <dfcv126@gmail.com>
2014-01-22 17:54:46 -0800

    Ch 9 - Pt 2

--- src/test/java/com/javafortesters/basicsofjavarevisited/ArraysAndLoopingTest.java
+++ src/test/java/com/javafortesters/basicsofjavarevisited/ArraysAndLoopingTest.java
@@ -128,4 +128,37 @@ public class ArraysAndLoopingTest {
         assertEquals(null, weekDays[3]);
         assertEquals(null, weekDays[4]);
     }
+
+    @Test
+    public void arraysFill() {
+
+        int[] tenItems = {0,0,0,0,0,1,1,1,1,1};
+
+        Arrays.fill(tenItems, 5, 10, 2);
+
+        assertEquals(2, tenItems[5]);
+        assertEquals(2, tenItems[6]);
+        assertEquals(2, tenItems[7]);
+        assertEquals(2, tenItems[8]);
+        assertEquals(2, tenItems[9]);
+    }
+
+    @Test
+    public void arraysSort() {
+
+        int[] outOfOrder = {9,7,8,2,4,3,6,1,5,0};
+
+        Arrays.sort(outOfOrder);
+
+        assertEquals(0, outOfOrder[0]);
+        assertEquals(1, outOfOrder[1]);
+        assertEquals(2, outOfOrder[2]);
+        assertEquals(3, outOfOrder[3]);
+        assertEquals(4, outOfOrder[4]);
+        assertEquals(5, outOfOrder[5]);
+        assertEquals(6, outOfOrder[6]);
+        assertEquals(7, outOfOrder[7]);
+        assertEquals(8, outOfOrder[8]);
+        assertEquals(9, outOfOrder[9]);
+    }
 }
