4184f7b5f9f412e04eac5c31358a084af3ab3c41
David Forshaw <dfcv126@gmail.com>
2014-01-27 11:31:22 -0800

    Ch 10 Collections Exercises -- part 2

--- src/test/java/com/javafortesters/basicsofjavarevisited/CollectionTest.java
+++ src/test/java/com/javafortesters/basicsofjavarevisited/CollectionTest.java
@@ -1,6 +1,7 @@
 package com.javafortesters.basicsofjavarevisited;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,10 +10,15 @@ import java.util.Collection;
 
 public class CollectionTest {
 
-    Collection<String> workdays = new <String>ArrayList();
+    Collection<String> workdays;
+
+    @Before
+    public void setUp()  {
+        workdays = new <String>ArrayList();
+    }
 
     @Test
-    public void Collection_add_size() {
+    public void Collection__add__size() {
 
         workdays.add("Monday");
         workdays.add("Tuesday");
@@ -21,5 +27,34 @@ public class CollectionTest {
         workdays.add("Friday");
 
         Assert.assertEquals(5, workdays.size());
+    }
+
+    @Test
+    public void Collection__add_all__size__containsAll() {
+
+        Collection<String> daysOfWeek = new <String>ArrayList();
+
+        daysOfWeek.addAll(workdays);
+
+        Assert.assertEquals(workdays.size(), daysOfWeek.size());
+        Assert.assertTrue(daysOfWeek.containsAll(workdays));
+    }
+
+    @Test
+    public void Collection__remove__contains() {
+        Collection<String> weekendDays = new <String>ArrayList();
+
+        weekendDays.add("Saturday");
+        weekendDays.add("Funday");
+
+        weekendDays.remove("Funday");
+
+        Assert.assertFalse(weekendDays.contains("Funday"));
+
+        weekendDays.add("Sunday");
+
+        Assert.assertEquals(2, weekendDays.size());
+        Assert.assertTrue("Bug Fixed, Sunday is in the collection now",
+                weekendDays.contains("Sunday"));
     }
 }
