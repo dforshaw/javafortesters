e506a9ecb272db80431722ff00b1719a4590c546
David Forshaw <dfcv126@gmail.com>
2014-01-28 16:59:38 -0800

    Ch 11 Exceptions - Exercises Pt 3

--- src/test/java/com/javafortesters/exceptions/ExceptionsExampleTest.java
+++ src/test/java/com/javafortesters/exceptions/ExceptionsExampleTest.java
@@ -93,5 +93,44 @@ public class ExceptionsExampleTest {
         Integer age = null;
         age.toString();
     }
+
+    @Test
+    public void tryCatchFinallyNullPointerException() {
+        Integer age = null;
+        String ageAsString;
+        String yourAge = "";
+
+        try {
+            ageAsString = age.toString();
+
+        } catch (NullPointerException e) {
+            age = 18;
+            ageAsString = age.toString();
+
+        } finally {
+            yourAge = "You are " + age.toString() + " years old.";
+
+        }
+
+        assertEquals("You are 18 years old.", yourAge);
+    }
+
+//    @Test (expected = IllegalArgumentException.class)
+    @Test
+    public void exampleTryCatchFinally() {
+        Integer age = null;
+
+        try {
+            System.out.println("1. generate a null pointer exception");
+            System.out.println(age.toString());
+
+        } catch (NullPointerException e) {
+            System.out.println("2. handle null pointer exception");
+            throw new IllegalArgumentException("Null pointer became illegal", e);
+
+        } finally {
+            System.out.println("3. run code in finally section");
+        }
+    }
 }
 
