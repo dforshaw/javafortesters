4b0e121d02c723ed9c519f8fe11c58a9e919ed16
David Forshaw <dfcv126@gmail.com>
2014-01-28 16:46:52 -0800

    Ch 11 Exceptions - Exercises Pt 2

--- src/test/java/com/javafortesters/exceptions/ExceptionsExampleTest.java
+++ src/test/java/com/javafortesters/exceptions/ExceptionsExampleTest.java
@@ -19,15 +19,18 @@ public class ExceptionsExampleTest {
 
         assertEquals("You are 18 years old.", yourAge);
     }
-    @Test
-    public void throwANullPointerException_tryCatch() {
-        Integer age = null;
 
+    @Test
+    public void catchANullPointerException() {
+        Integer age = null;
         String ageAsString;
 
         try {
             ageAsString = age.toString();
+
+//        } catch (ArithmeticException e) {
         } catch (NullPointerException e) {
+//        } catch (NoSuchMethodException e) {
             age = 18;
             ageAsString = age.toString();
         }
@@ -35,6 +38,60 @@ public class ExceptionsExampleTest {
         String yourAge = "You are " + ageAsString + " years old.";
 
         assertEquals("You are 18 years old.", yourAge);
+    }
+
+    @Test
+    public void catchANullPointerExceptionWithMessages() {
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+
+        } catch (NullPointerException e) {
+
+            System.out.println("getMessage - " + e.getMessage());
+            System.out.println("getStackTrace - " + e.getStackTrace());
+
+            // Find out what the proper return type would be for printStackTrace()
+            // System.out.println("printStackTrace - " + e.printStackTrace());
+
+            age = 18;
+            ageAsString = age.toString();
+        }
+
+        String yourAge = "You are " + ageAsString + " years old.";
+
+        assertEquals("You are 18 years old.", yourAge);
+    }
+
+    @Test
+    public void catchMoreThanOneException() {
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+
+        } catch (NullPointerException e) {
+
+            age = 18;
+            ageAsString = age.toString();
+
+        } catch (IllegalArgumentException e) {
+
+            System.out.println("Illegal Argument: " + e.getMessage());
+        }
+
+        //String yourAge = "You are " + ageAsString + " years old.";
+
+        //assertEquals("You are 18 years old.", yourAge);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void nullPointerExceptionExpected() {
+        Integer age = null;
+        age.toString();
     }
 }
 
