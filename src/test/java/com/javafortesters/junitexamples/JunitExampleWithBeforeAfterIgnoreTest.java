631113faa61968f63d7350029dbb053a93cd77ec
David Forshaw <dfcv126@gmail.com>
2014-02-02 09:25:39 -0800

    Ch 14 JUnit Examples - Pt 3

---
+++ src/test/java/com/javafortesters/junitexamples/JunitExampleWithBeforeAfterIgnoreTest.java
@@ -0,0 +1,42 @@
+package com.javafortesters.junitexamples;
+
+import org.junit.*;
+
+public class JunitExampleWithBeforeAfterIgnoreTest {
+
+    @BeforeClass
+    public static void runOncePerClassBeforeAnyTests() {
+        System.out.println("@BeforeClass method");
+    }
+
+    @AfterClass
+    public static void runOncePerClassAfterAllTests() {
+        System.out.println("@AfterClass method");
+    }
+
+    @Before
+    public void runBeforeEveryTestMethod() {
+        System.out.println("@Before each method");
+    }
+
+    @After
+    public void runAfterEveryTestMethod() {
+        System.out.println("@After each method");
+    }
+
+    @Test
+    public void method1() {
+        System.out.println("Method 1");
+    }
+
+    @Test
+    public void method2() {
+        System.out.println("Method 2");
+    }
+
+    @Ignore("Finish this -- still incomplete")
+    @Test
+    public void methodToIgnore() {
+        System.out.println("Ignore this method");
+    }
+}
