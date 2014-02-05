6e7990b781d5cb4b684894af6f670ccec98f39c2
David Forshaw <dfcv126@gmail.com>
2014-02-04 17:11:10 -0800

    Ch 14 Junit Assertions - Hamcrest - refactoring

--- src/test/java/com/javafortesters/junitexamples/hamcrestassertions/HamcrestAssertThatIs.java
+++ src/test/java/com/javafortesters/junitexamples/hamcrestassertions/HamcrestAssertThatIs.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class JunitHamcrestAssertThatIs {
+public class HamcrestAssertThatIs {
 
     @Test
     public void AssertThatIsTest() {
