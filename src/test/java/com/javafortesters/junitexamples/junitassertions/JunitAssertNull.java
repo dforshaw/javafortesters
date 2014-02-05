433507997761288a1dad4877bf9930e98bf2043d
David Forshaw <dfcv126@gmail.com>
2014-02-04 16:37:33 -0800

    Ch 14 Junit Assertions - more types

--- src/test/java/com/javafortesters/junitexamples/junitassertions/JunitAssertNull.java
+++ src/test/java/com/javafortesters/junitexamples/junitassertions/JunitAssertNull.java
@@ -1,4 +1,26 @@
 package com.javafortesters.junitexamples.junitassertions;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
 public class JunitAssertNull {
+
+    String notNull = "a";
+    String isNull = null;
+
+    @Test
+    public void AssertNullTest() {
+        assertNull(isNull);
+    }
+
+    @Test
+    public void AssertNullWithMessageTest() {
+        assertNull("This is null", isNull);
+    }
+
+    @Test  (expected = AssertionError.class)
+    public void AssertNullFailureWithMessageTest() {
+        assertNull("This might not be null", notNull);
+    }
 }
