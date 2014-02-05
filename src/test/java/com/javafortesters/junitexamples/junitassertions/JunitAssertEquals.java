5ef60a1fd417d9ab567e145c9b850cac31a68c60
David Forshaw <dfcv126@gmail.com>
2014-02-04 16:29:58 -0800

    Ch 14 Junit Assertions - refactoring

--- src/test/java/com/javafortesters/junitexamples/junitassertions/JunitAssertEquals.java
+++ src/test/java/com/javafortesters/junitexamples/junitassertions/JunitAssertEquals.java
@@ -8,17 +8,17 @@ import static org.junit.Assert.*;
 public class JunitAssertEquals {
 
     @Test
-    public void AssertionExample() {
+    public void AssertEqualsTest() {
         assertEquals(6, 3 + 3);
     }
 
     @Test
-    public void AssertionWithMessageTest() {
+    public void AssertEqualsWithMessageTest() {
         assertEquals("3 + 3 = 6", 6, 3 + 3);
     }
 
     @Test  (expected = AssertionError.class)
-    public void AssertionFailureWithMessageTest() {
+    public void AssertEqualsFailureWithMessageTest() {
         assertEquals("3 + 3 = 6", 5, 3 + 3);
     }
 }
