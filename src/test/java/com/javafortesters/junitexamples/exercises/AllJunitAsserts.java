30bbd2239394a45a5811e0f997915dbdaa5dcb7b
David Forshaw <dfcv126@gmail.com>
2014-02-28 23:16:05 -0800

    Ch 14 Junit Assertions - refactored exercise

--- src/test/java/com/javafortesters/junitexamples/exercises/AllJunitAsserts.java
+++ src/test/java/com/javafortesters/junitexamples/exercises/AllJunitAsserts.java
@@ -17,15 +17,50 @@ public class AllJunitAsserts {
     String isNull = null;
 
     @Test
-    public void AssertTests() {
+    public void AssertTests_assertArrayEquals() {
 
         assertArrayEquals(testArray, workdays);
+    }
+
+    @Test
+    public void AssertTests_assertEquals() {
+
         assertEquals(phrase1, phrase2);
+    }
+
+    @Test
+    public void AssertTests_assertFalse() {
+
         assertFalse(falsey);
+    }
+
+    @Test
+    public void AssertTests_assertTrue() {
+
         assertTrue(truthy);
+    }
+
+    @Test
+    public void AssertTests_assertSame() {
+
         assertSame(phrase1, phrase2);
+    }
+
+    @Test
+    public void AssertTests_assertNotSame() {
+
         assertNotSame(phrase1, phrase3);
+    }
+
+    @Test
+    public void AssertTests_assertNull() {
+
         assertNull(isNull);
+    }
+
+    @Test
+    public void AssertTests_assertNotNull() {
+
         assertNotNull(notNull);
     }
 }
