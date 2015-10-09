561eef65408d235b4b45c9c3249c1bee5a046fd8
David Forshaw <dfcv126@gmail.com>
2015-10-09 14:33:52 -0700

    added string tests

---
+++ src/test/java/com/javafortesters/strings/stringTestToString.java
@@ -0,0 +1,28 @@
+package com.javafortesters.strings;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by david on 10/9/15.
+ */
+public class stringTestToString {
+
+    @Test
+    public void stringConvertTest1() {
+
+        String intConcatConvert = "" + 1;
+
+        assertThat(intConcatConvert, is("1"));
+    }
+
+    @Test
+    public void stringConvertTest2() {
+
+        String intIntConvert = Integer.toString(2);
+
+        assertThat(intIntConvert, is("2"));
+    }
+}
---
+++ src/test/java/com/javafortesters/strings/stringTestValueOf.java
@@ -0,0 +1,38 @@
+package com.javafortesters.strings;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by david on 10/9/15.
+ */
+public class stringTestValueOf {
+
+    @Test
+    public void stringConvertTest3() {
+
+        String integerStringConvert = String.valueOf(3);
+
+        assertThat(integerStringConvert, is("3"));
+    }
+
+    @Test
+    public void stringConvertTest4() {
+
+        char[] cArray = {'2', '3'};
+
+        assertThat("23".toCharArray(), is(cArray));
+    }
+
+    @Test
+    public void canConvertBytesUTF8() throws UnsupportedEncodingException{
+
+        byte[] b8Array = "hello there".getBytes("UTF8");
+
+        System.out.println(b8Array.length);
+    }
+}
