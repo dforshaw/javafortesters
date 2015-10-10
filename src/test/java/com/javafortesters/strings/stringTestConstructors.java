41b8d12788aa738de4a3b79808f522f3aa772e5c
David Forshaw <dfcv126@gmail.com>
2015-10-09 18:02:05 -0700

    added string tests

---
+++ src/test/java/com/javafortesters/strings/stringTestConstructors.java
@@ -0,0 +1,28 @@
+package com.javafortesters.strings;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by david on 10/9/15.
+ */
+public class stringTestConstructors {
+
+    @Test
+    public void stringConstructorTestEmpty() {
+
+        String empty = new String();
+
+        assertThat(empty.length(), is(0));
+    }
+
+    @Test
+    public void stringConstructorTestChar() {
+
+        char[] cArray = {'2', '3'};
+
+        assertThat(new String(cArray), is("23"));
+    }
+}
