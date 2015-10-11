500771bfa8ac17da3e8e272e6cb25f7fc619cbfd
David Forshaw <dfcv126@gmail.com>
2015-10-11 14:38:22 -0700

    Add string tests

--- src/test/java/com/javafortesters/strings/stringTestComparisons.java
+++ src/test/java/com/javafortesters/strings/stringTestComparisons.java
@@ -10,10 +10,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class stringTestComparisons {
 
+    String hello = "Hello";
+
     @Test
     public void stringComparisonTestCompareTo() {
-
-        String hello = "Hello";
 
         assertThat(hello.compareTo("Hello"), is(0));
     }
@@ -21,15 +21,11 @@ public class stringTestComparisons {
     @Test
     public void stringComparisonTestCompareToNeg1() {
 
-        String hello = "Hello";
-
         assertThat(hello.compareTo("hello") < 0, is(true));
     }
 
     @Test
     public void stringComparisonTestCompareToNeg2() {
-
-        String hello = "Hello";
 
         assertThat(hello.compareTo("Helloo") < 0, is(true));
     }
@@ -37,15 +33,11 @@ public class stringTestComparisons {
     @Test
     public void stringComparisonTestCompareToNeg3() {
 
-        String hello = "Hello";
-
         assertThat(hello.compareTo("Hemlo") < 0, is(true));
     }
 
     @Test
     public void stringComparisonTestCompareToPos1() {
-
-        String hello = "Hello";
 
         assertThat(hello.compareTo("H") > 0, is(true));
     }
@@ -53,15 +45,11 @@ public class stringTestComparisons {
     @Test
     public void stringComparisonTestCompareToPos2() {
 
-        String hello = "Hello";
-
         assertThat(hello.compareTo("Helln") > 0, is(true));
     }
 
     @Test
     public void stringComparisonTestCompareToPos3() {
-
-        String hello = "Hello";
 
         assertThat(hello.compareTo("HeLlo") > 0, is(true));
     }
@@ -69,15 +57,11 @@ public class stringTestComparisons {
     @Test
     public void stringComparisonTestCompareToIgnoreCase1() {
 
-        String hello = "Hello";
-
         assertThat(hello.compareToIgnoreCase("hello"), is(0));
     }
 
     @Test
     public void stringComparisonTestCompareToIgnoreCase2() {
-
-        String hello = "Hello";
 
         assertThat(hello.compareToIgnoreCase("Hello"), is(0));
     }
@@ -85,8 +69,112 @@ public class stringTestComparisons {
     @Test
     public void stringComparisonTestCompareToIgnoreCase3() {
 
-        String hello = "Hello";
-
         assertThat(hello.compareToIgnoreCase("HeLlo"), is(0));
+    }
+
+    @Test
+    public void stringComparisonTestContains1() {
+
+        assertThat(hello.contains("He"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestContains2() {
+
+        assertThat(hello.contains("Hello"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestContains3() {
+
+        assertThat(hello.contains("LL"), is(false));
+    }
+
+    @Test
+    public void stringComparisonTestContains4() {
+
+        assertThat(hello.contains("z"), is(false));
+    }
+
+    @Test
+    public void stringComparisonTestContentEquals1() {
+
+        assertThat(hello.contentEquals("Hello"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestContentEquals2() {
+
+        assertThat(hello.contentEquals("hello"), is(false));
+    }
+
+    @Test
+    public void stringComparisonTestEqualsIgnoreCase() {
+
+        assertThat(hello.equalsIgnoreCase("hello"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestEndsWith1() {
+
+        assertThat(hello.endsWith("Hello"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestEndsWith2() {
+
+        assertThat(hello.endsWith(""), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestEndsWith3() {
+
+        assertThat(hello.endsWith("lo"), is(true));
+    }
+
+
+    @Test
+    public void stringComparisonTestEndsWith4() {
+
+        assertThat(hello.endsWith("Lo"), is(false));
+    }
+    @Test
+    public void stringComparisonTestStartsWith1() {
+
+        assertThat(hello.startsWith("Hello"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestStartsWith2() {
+
+        assertThat(hello.startsWith(""), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestStartsWith3() {
+
+        assertThat(hello.startsWith("He"), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestStartsWith4() {
+
+        assertThat(hello.startsWith("he"), is(false));
+    }
+
+    @Test
+    public void stringComparisonTestEmpty1() {
+
+        String empty = "";
+
+        assertThat(empty.isEmpty(), is(true));
+    }
+
+    @Test
+    public void stringComparisonTestEmpty2() {
+
+        String empty = "";
+
+        assertThat(empty.length(), is(0));
     }
 }
