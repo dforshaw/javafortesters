a361c19fe616cc437082cd634ca023f6ffb0548e
David Forshaw <dfcv126@gmail.com>
2015-10-30 16:53:10 -0700

    updates - StringBuilder examples & exercises

--- src/test/java/com/javafortesters/strings/stringBuilderTestExamples.java
+++ src/test/java/com/javafortesters/strings/stringBuilderTestExamples.java
@@ -127,4 +127,141 @@ public class stringBuilderTestExamples {
 
 //        assertThat(builder.toString(), is("1234567890"));
     }
+
+    @Test
+    public void stringBuilderTestExercise2() {
+
+        //part 1
+        System.out.println("Part 1");
+        StringBuilder builder = new StringBuilder();
+        int beforeLength = builder.length();
+        System.out.println("Before: " + beforeLength);
+        assertThat("intial length of empty is 16", beforeLength, is(0));
+
+        String phrase = "new phrase to insert";
+        System.out.println("Phrase Length: " + phrase.length());
+
+        builder.insert(beforeLength, phrase);
+        int afterLength = builder.length();
+        System.out.println("After: " + afterLength);
+        assertThat("post-insert length of empty is 16", afterLength, is(beforeLength + phrase.length()));
+
+        System.out.println(builder.toString());
+
+        int insertPoint = afterLength;
+
+        //part 2
+        System.out.println("\n" + "Part 2");
+        String phraseAtEnd = " | new phrase to insert at the end";
+        beforeLength = builder.length();
+        System.out.println("Before: " + beforeLength);
+        System.out.println("Phrase Length: " + phraseAtEnd.length());
+        builder.append(phraseAtEnd);
+        afterLength = builder.length();
+        System.out.println("After: " + afterLength);
+        assertThat("post-append length of phrase", afterLength, is(beforeLength + phraseAtEnd.length()));
+
+        System.out.println(builder.toString());
+
+        //part 3
+        System.out.println("\n" + "Part 3");
+        String phraseInMiddle = " | new phrase to insert in the middle";
+        beforeLength = builder.length();
+        System.out.println("Before: " + beforeLength);
+        System.out.println("Phrase Length: " + phraseInMiddle.length());
+        builder.insert(insertPoint, phraseInMiddle);
+        afterLength = builder.length();
+        System.out.println("After: " + afterLength);
+        assertThat("post-insert length of phrase", afterLength, is(beforeLength + phraseInMiddle.length()));
+
+        System.out.println(builder.toString());
+    }
+
+    @Test
+    public void stringBuilderTestInsertChar() {
+
+        char[] ca = {'.', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        StringBuilder builder = new StringBuilder("abgh");
+        System.out.println(builder.toString());
+
+        builder.insert(2, ca, 3, 4);
+        System.out.println(builder.toString());
+
+        assertThat(builder.toString(), is ("abcdefgh"));
+    }
+
+    @Test
+    public void stringBuilderTestDelete() {
+
+        StringBuilder builder = new StringBuilder("abcdefg");
+        System.out.println(builder.toString());
+
+        builder.delete(2, 4);
+        System.out.println(builder.toString());
+
+        assertThat(builder.toString(), is ("abefg"));
+    }
+
+    @Test
+    public void stringBuilderTestDeleteCharAt() {
+
+        StringBuilder builder = new StringBuilder("abcdefg");
+        System.out.println(builder.toString());
+
+        builder.deleteCharAt(2);
+        System.out.println(builder.toString());
+
+        assertThat(builder.toString(), is ("abdefg"));
+    }
+
+    @Test
+    public void stringBuilderTestReplace() {
+
+        StringBuilder builder = new StringBuilder("abcdefgh");
+        System.out.println(builder.toString());
+
+        builder.replace(0, 4, "12345678");
+        System.out.println(builder.toString());
+
+        assertThat(builder.toString(), is ("12345678efgh"));
+    }
+
+    @Test
+    public void stringBuilderTestSetCharAt() {
+
+        StringBuilder builder = new StringBuilder("012345678");
+        System.out.println(builder.toString());
+
+        builder.setCharAt(5, 'f');
+        System.out.println(builder.toString());
+
+        assertThat(builder.toString(), is ("01234f678"));
+    }
+
+    @Test
+    public void stringBuilderTestReverse() {
+
+        StringBuilder builder = new StringBuilder("0123456789");
+        System.out.println(builder.toString());
+
+        assertThat(builder.reverse().toString(), is ("9876543210"));
+
+        System.out.println(builder.toString());
+    }
+
+    @Test
+    public void stringBuilderTestSubstring() {
+
+        StringBuilder builder = new StringBuilder("0123456789");
+        System.out.println(builder.toString());
+
+        System.out.println("Partial substring: " + builder.substring(3, 7).toString());
+
+        assertThat(builder.substring(3, 7).toString(), is("3456"));
+
+        System.out.println("Substring from a start index: " + builder.substring(3).toString());
+
+        assertThat(builder.substring(3).toString(), is ("3456789"));
+    }
 }
