883231f1018a67864931569f8d991d92f8d9f547
David Forshaw <dfcv126@gmail.com>
2015-10-30 17:09:44 -0700

    updates - Random Data examples & exercises

--- src/test/java/com/javafortesters/randomdata/randomDataTestExamples.java
+++ src/test/java/com/javafortesters/randomdata/randomDataTestExamples.java
@@ -1,7 +1,23 @@
 package com.javafortesters.randomdata;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by david on 10/29/15.
  */
 public class randomDataTestExamples {
+
+    @Test
+    public void randomDataTestMathRandom1() {
+
+        double rnd = Math.random();
+
+        System.out.println(String.format("generated %f as random number", rnd));
+
+        assertThat(rnd < 1.0d, is(true));
+        assertThat(rnd >= 0.0d, is(true));
+    }
 }
---
+++ src/test/java/com/javafortesters/randomdata/randomDataTestExercises.java
@@ -0,0 +1,34 @@
+package com.javafortesters.randomdata;
+
+import org.junit.Test;
+
+/**
+ * Created by david on 10/30/15.
+ */
+public class randomDataTestExercises {
+
+    @Test
+    public void randomDataTestExercise1() {
+
+    }
+
+    @Test
+    public void randomDataTestExercise2() {
+
+    }
+
+    @Test
+    public void randomDataTestExercise3() {
+
+    }
+
+    @Test
+    public void randomDataTestExercise4() {
+
+    }
+
+    @Test
+    public void randomDataTestExercise5() {
+
+    }
+}
