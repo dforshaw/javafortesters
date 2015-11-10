4c4656beede90d2d2c3804b659eb87dd3ee669bf
David Forshaw <dfcv126@gmail.com>
2015-11-09 23:38:05 -0800

    updates - Random Data examples & exercises

--- src/test/java/com/javafortesters/randomdata/randomDataTestExercises.java
+++ src/test/java/com/javafortesters/randomdata/randomDataTestExercises.java
@@ -2,14 +2,36 @@ package com.javafortesters.randomdata;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by david on 10/30/15.
  */
 public class randomDataTestExercises {
 
+    // Create @Test Methods Which Confirm Random Limits
+
     @Test
     public void randomDataTestExercise1() {
 
+        ArrayList<Integer> intList = new ArrayList();
+        Random generate = new Random();
+
+        for (int i=0; i<1000; i++) {
+
+            intList.add(i,generate.nextInt());
+
+            System.out.println(
+                    String.format("# %d: generated %d as random value", i, intList.get(i))
+            );
+
+            assertThat(intList.get(i) < Integer.MAX_VALUE, is(true));
+            assertThat(intList.get(i) >= Integer.MIN_VALUE, is(true));
+        }
     }
 
     @Test
